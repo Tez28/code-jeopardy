2cbfad9ec91a5cefc688b4a970d92cc110d0f8da
Tez28 <courtezcannady@gmail.com>
2022-01-08 12:49:32 -0500

    functioning timer

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,24 @@
+// declare variables
 var startButton = document.querySelector(".start-btn")
 var timeEl = document.querySelector(".time") 
 var choicesEl   = document.querySelector(".choices")
 var questionsEl = document.querySelector("#questions")
 var inititalsEl = document.querySelector("#initials")
+
+// time variables
+
+//seconds starting-left
+var secondsLeft = 75;
+
+// hold time for interval
+var holdTimer = 0;
+
+//input penalty time
+var penalty = 15;
+
+
+
+
 var questions = [
     {
         Title: "Question 1",
@@ -67,3 +83,20 @@ function startQuiz () {
 
 
 startButton.onclick = startQuiz
+
+//trigger timer on start button
+startButton.addEventListener("click", function (){
+
+    if(holdTimer === 0) {
+        holdTimer = setInterval(function(){
+            secondsLeft--;
+            timeEl.textContent = "Time: " + secondsLeft;
+
+            if(secondsLeft <= 0) {
+                clearInterval(holdTimer);
+                quizDone();
+                timeEl,textContent = "Time is up! Check score";
+            }
+        }, 1000);
+    }
+})
