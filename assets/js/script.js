62d95fe2a5bd0556bff06c0765c970da860a9b6b
Tez28 <courtezcannady@gmail.com>
2022-01-11 18:07:42 -0500

    adds initial leader board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,29 @@
 // declare variables
+
 var startButton = document.querySelector(".start-btn")
 var timeEl = document.querySelector(".time") 
 var choicesEl   = document.querySelector(".choices")
-var questionsEl = document.querySelector("#questions")
-var inititalsEl = document.querySelector("#initials")
-var questionsTi = document.querySelector(".questions-title")
-var createUl = document.createElement("ul")
+var nextBtn = document.getElementById("#next-btn")
+var questionsEl = document.querySelector("#questions");
+var answersEl = document.querySelector("#answers")
+var inititalsEl = document.querySelector("#initials");
+var questionsTi = document.querySelector(".question-title");
 
-// question numer variables
-var currentQue = 0;
-//seconds starting-left
-var secondsLeft = 75;
+var finalScore = document.getElementById(".final-score");
 
-// hold time for interval
-var holdTimer = 0;
+var checkAnswer = document.getElementById("checkAnswer")
 
-//input penalty time
+var holdTimer;
+var time = 75;
 var penalty = 15;
-
-// add score variable
 var scoreEl = 0;
-var questionsAns = 0;
+var correctAns = 0;
+var secondsLeft = "";
+var questionIndex = 0;
 
 
 
-
-var questions = [
+const questions = [
     {
         Title: "Question 1",
         choices: ["A", "B", "C", "D" ],
@@ -37,92 +35,120 @@ var questions = [
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 3",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 4",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 5",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 6",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 7",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 8",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 9",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
     {
-        Title: "Question 1",
+        Title: "Question 10",
         choices: ["A", "B", "C", "D" ],
         answer: "A"
     },
 ]
+
+
 // add event listener for start quiz
+//trigger timer on start button
 
 function startQuiz () {
-    console.log("Quiz Started")
-    var startEl = document.querySelector(".start")
-    startEl.setAttribute("class", "hide")
-    questionsEl.removeAttribute("class")
+    console.log("Quiz Started");
+    var startEl = document.querySelector(".start");
+    startEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    questionIndex = 0;
+    holdTimer = setInterval(clocktime, 1000);
+    timeEl.textContent = time
+    initiateQuest();
+
+}
+function clocktime() {
+    time --;
+    timeEl.textContent = time
+    if (time <= 0) {
+        timeEl.textContent = "Time is up! Check score";
+        endQuiz();
+    }
 
 }
 
-
-startButton.onclick = startQuiz
-
-//trigger timer on start button
-startButton.addEventListener("click", function (){
-
-    if(holdTimer === 0) {
-        holdTimer = setInterval(function(){
-            secondsLeft--;
-            timeEl.textContent = + secondsLeft;
-
-            if(secondsLeft <= 0) {
-                clearInterval(holdTimer);
-                timeEl.textContent = "Time is up! Check score";
-            }
-        }, 1000);
-    }
-});
-
-// render question to page
-function render (currentQue) {
-    questionsEl.innerHTML = "";
-    createUl.innerHTML = "";
-
-    for (var i = 0; i < questions.length; i++) {
-
-        var userQuestions = questions[currentQue].Title;
-        var userOptions = questions[currentQue].choices;
-        questionsEl.textContent = userQuestions;
+// present questions title and choices
+function initiateQuest() {
+    var currentQuestion = questions[questionIndex]
+    questionsTi.textContent = currentQuestion.Title;
+    choicesEl.innerHTML = "";
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+        var choiceBtn = document.createElement("button")
+        choiceBtn.setAttribute("class", "classChoices")
+        choiceBtn.setAttribute("value", currentQuestion.choices[i])
+        choiceBtn.textContent = i+ 1 + ": " + currentQuestion.choices[i]
+        choiceBtn.onclick = nextQuestion
+        choicesEl.appendChild(choiceBtn)
     }
 
-    userOptions.forEach(function (newItem) {
-        var listItem = document.createElement("li");
-        listItem.textContent = newItem;
-        questionsEl.appendChild(listItem);
-        listItem.addEventListener("click", (compare));
-    })
+}
+// checks answers for correct or incorrect
+function nextQuestion() {
+    console.log("inside next question")
+    if ( this.value === questions[questionIndex].answer) {
+        // add 1pt for correct
+        correctAns++;
+        // inform tester correct
+        checkAnswer.textContent = "Your Correct!";
+    } else {
+        // if wrong answer selected inform and subtract time
+        time -= 15;
+        timeEl.textContent = time;
+        checkAnswer.textContent = "Incorrect";
+    }
+
+    questionIndex++;
+    // this repeats sequence for all questions
+    if (questionIndex === questions.length) {
+        endQuiz();
+    }else {
+        // once final question reached end quiz
+        initiateQuest();
+    }
+
+}
+
+startButton.onclick = startQuiz;
+
+//ends quiz and takes to final leader board
+function endQuiz() {
+    console.log ("Quiz has Ended!");
+    var endEl = document.getElementById("end-screen");
+    questionsEl.setAttribute("class" , "hide");
+    endEl.removeAttribute("class");
 
 }
